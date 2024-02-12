41059e2312554115addc72ae54200da30ddcb1ed
kwlee <kwlee@etri.re.kr>
2024-02-12 16:54:41 +0900

    .

--- setup_ansible.sh
+++ setup_ansible.sh
@@ -2,5 +2,4 @@
 
 ansible-playbook desktop/generate_ssh_key.yml
 ansible-playbook base/allow_sudoer.yml
-ansible-playbook base/collect_ssh_pub_keys.yml
-ansible-playbook base/deploy_ssh_pub_key.yml
+ansible-playbook base/ssh_connection.yml
