fc658b5948d21cd59cb7a5fa94faadbe15e0f0a0
Kang-Woo Lee <kwlee@etri.re.kr>
2024-02-20 14:44:52 +0900

    .

--- kubernetes/install_ansible.sh
+++ kubernetes/install_ansible.sh
@@ -2,7 +2,6 @@
 
 ansible-playbook base/ssh_connection.yml --limit k8s -K
 ansible k8s -m ping
-
 ansible-role sudo --hosts k8s -K
+
 ansible-playbook kubernetes/main.yml
-ansible-playbook kubernetes/add_k8s_user.yml
