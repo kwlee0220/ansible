48041e0d5b7b5f3e0e8c4992eebcd6474e78f964
Kang-Woo Lee <kwlee@etri.re.kr>
2025-07-02 07:58:13 +0900

    MDT 환경 설정 script 추가

---
+++ playbooks/mdt/files/models/add_models.sh
@@ -0,0 +1,14 @@
+#!	/bin/bash
+
+mdt add test $MDT_HOME/models/test
+
+mdt add welder $MDT_HOME/models/welder
+
+mdt add inspector $MDT_HOME/models/innercase/inspector
+mdt add heater $MDT_HOME/models/innercase/heater
+mdt add trimmer $MDT_HOME/models/innercase//trimmer
+mdt add former $MDT_HOME/models/innercase/former
+mdt add innercase $MDT_HOME/models/innercase/innercase
+
+mdt add panda $MDT_HOME/models/panda
+mdt add opcua $MDT_HOME/models/opcua
