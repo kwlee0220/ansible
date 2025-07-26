b09e02be4afe7253ff83ebaf1c3188f02ad46528
Kang-Woo Lee <kwlee@etri.re.kr>
2025-07-26 17:14:42 +0900

    .

--- playbooks/argo/start_argo_workflow.sh
+++ playbooks/argo/start_argo_workflow.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-#kubectl patch deployment -n argo argo-server --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/args", "value": ["server", "--auth-mode=server"]}]'
+minikube start --memory=no-limit --cpus=no-limit
 kubectl -n argo port-forward deployment/argo-server 2746:2746 --address 0.0.0.0
-#argo server --auth-mode=server
